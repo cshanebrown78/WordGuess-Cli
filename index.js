a06c0aeef81e452b809ab5ed3f2a3a09a93b9887
cshanebrown78 <cshanebrown78@gmail.com>
2020-04-07 15:23:58 -0500

    started coding index.html file

---
+++ index.js
@@ -0,0 +1,7 @@
+var Word = require("./word.js");
+var inquirer = require("inquirer");
+
+var wordGrab = 0;
+var wordChosen = "";
+var wordConvert = "";
+var cityList = ["dallas", "austin", "san antonio", "houston", "los angeles", "new orleans", "chicago", "new york", "boston", "atlanta", "miami", "denver", "portland"]
--- word.js
+++ word.js
@@ -8,8 +8,16 @@ function Word(word) {
         for (var i = 0; i < this.word.length; i++) {
             this.wordArr.push(new Letter(this.word[i]));
             // this.wordArr.push(character);
+            // this.wordArr[i].toString();
         }
-        console.log(this.wordArr);
+        
+    }
+    this.displayWord = function() {
+        toDisplay = [];
+        for (var j = 0; j < this.wordArr.length; j++) {
+            toDisplay.push(this.wordArr[i].toString());
+        }
+        console.log(toDisplay.join(" "));
     }
     this.letterGuess = function(userInput){
         for (var j = 0; j < this.wordArr; j++) {
@@ -19,5 +27,7 @@ function Word(word) {
 }
 
 
-var testWord = new Word("test");
-testWord.wordBuild();
+    var testWord = new Word("test");
+    testWord.wordBuild();
+    
+
