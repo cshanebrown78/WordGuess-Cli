be80af601a9369c2843be3f528775a3815eaa97f
cshanebrown78 <cshanebrown78@gmail.com>
2020-04-08 16:09:22 -0500

    added gameRestart function and some other coding to gameStart

--- index.js
+++ index.js
@@ -5,23 +5,48 @@ var wordGrab = 0;
 var randWord = "";
 var wordConvert = "";
 var cityList = ["dallas", "austin", "san antonio", "houston", "los angeles", "new orleans", "chicago", "new york", "boston", "atlanta", "miami", "denver", "portland"]
-var guesses = 0;
+var guesses = 6;
 
 
 
 function gameStart() {
-    randWord = cityList[Math.floor(Math.random()*cityList.length)];
-    console.log(randWord);
-    wordConvert = new Word(randWord);
-    wordConvert.wordBuild();
-    console.log(wordConvert.displayWord());
-
-    cityList.splice(randWord,1);
-    // console.log(cityList)
-    
+    if(cityList.length>-1) {
+        randWord = cityList[Math.floor(Math.random()*cityList.length)];
+        // console.log(randWord);
+        wordConvert = new Word(randWord);
+        wordConvert.wordBuild();
+        console.log("\nYou have 6 wrong guesses to figure out the major US city.\n")
+        console.log(wordConvert.displayWord());
+        cityList.splice(randWord,1);
+    } esle {
+    console.log("/nThere are no cities left to guess.")
+        inquirer.prompt([
+            {
+                type: "checkbox",
+                name: "startOver",
+                message: "Do you want to start over?",
+                choices: ["Yes", "No"]
+            }
+        ])
+        .then(function(user) {
+            if(user.startOver === "Yes"){
+                gameRestart();
+            } else {
+                console.log("Thanks for playing...Goodbye!")
+            }
+        })
+    }
 }
 
 
 
 gameStart();
 
+function gameRestart() {
+    var wordGrab = 0;
+    var randWord = "";
+    var wordConvert = "";
+    var cityList = ["dallas", "austin", "san antonio", "houston", "los angeles", "new orleans", "chicago", "new york", "boston", "atlanta", "miami", "denver", "portland"]
+    var guesses = 6;
+    gameStart();
+}
